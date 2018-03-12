366329075bbc6fef48bfbaf0ac9f008db3be7168
Slash.Xin <slash.xin@gmail.com>
2018-03-12 21:34:49 +0800

    updated at ASUS

--- GetStartted/CNNForMinist/cnn_minist.py
+++ GetStartted/CNNForMinist/cnn_minist.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Project: CNN for minist
+# Project: CNN for mnist
 # Date:    2018/3/8 17:11 
 # Author:  Slash.Xin
 # Version: V1.0
@@ -19,7 +19,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
     # Input Layer
-    input_layer = tf.reshape(features['X'], [-1, 28, 28, 1])
+    input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(inputs=input_layer,
@@ -70,6 +70,39 @@ def cnn_model_fn(features, labels, mode):
     }
 
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+    # Load training and test data
+    mnist = tf.contrib.learn.datasets.load_dataset('mnist')
+    train_data = mnist.train.images # return np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images # return np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir='CNNForMinist/model')
+
+    # Set up a logging hook
+    tensors_to_log = {'probabilities':'softmax_tensor'}
+    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
+
+    # Train the Model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={'x': train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True
+    )
+    mnist_classifier.train(input_fn=train_input_fn, steps=20000, hooks=[logging_hook])
+
+    # Evaluate the Model
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={'x':eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False
+    )
 
 if __name__ == "__main__":
     tf.app.run()
